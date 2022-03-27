88b67240ed6f4697cef729bec53f6a5c9a738db0
V0D14ka <minecraft35510880@gmail.com>
2022-03-27 17:17:51 +1000

    all site

--- main/forms.py
+++ main/forms.py
@@ -2,7 +2,13 @@ from django import forms
 
 
 class EmailForm(forms.Form):
-    email = forms.EmailField(required=True)
-    subject = forms.CharField(label='Тема', required=True)
-    message = forms.CharField(label='Сообщение', widget=forms.Textarea, required=True)
+    email = forms.EmailField(label='', widget=forms.TextInput(attrs={'placeholder': 'Почта адресата',
+                                                                     'style': 'width: 250px;',
+                                                                     'class': 'form-control'}), required=True)
+    subject = forms.CharField(label='', widget=forms.TextInput(attrs={'placeholder': 'Тема',
+                                                                      'style': 'width: 250px;',
+                                                                      'class': 'form-control'}), required=True)
+    message = forms.CharField(label='', widget=forms.Textarea(attrs={'placeholder': 'Сообщение',
+                                                                     'style': 'width: 250px; height: 150px;',
+                                                                     'class': 'form-control'}), required=True)
 
