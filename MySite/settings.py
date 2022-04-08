51b23516076726c20472df5a6936f985b85d4194
V0D14ka <minecraft35510880@gmail.com>
2022-04-08 14:18:15 +1000

    all site

--- MySite/settings.py
+++ MySite/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 CSRF_TRUSTED_ORIGINS = ['https://www.vodichkawebsp.tk', 'https://*.127.0.0.1']
 ALLOWED_HOSTS = ['*']
 
