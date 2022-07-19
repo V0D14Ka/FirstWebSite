296036e1f3c3b3c71ceeb84a12def3e4c4fbfb8a
V0D14ka <minecraft35510880@gmail.com>
2022-07-20 03:15:51 +1000

    Added task to mailing

--- main/tasks.py
+++ main/tasks.py
@@ -6,10 +6,10 @@ from MySite.settings import EMAIL_HOST_USER
 
 @app.task
 def send_spam(owner, title):
-    subs = User.objects.get(id=owner).subscribers.all()
+    subs = User.objects.get(id=owner).friends.all()
     name = User.objects.get(id=owner).username
-    mesg = 'Здравствуйте. У пользователя ' + name + ' вышло новое видео - ' \
-           + '"' + title + '"' + '. С уважением, администрация SeaVid!'
+    mesg = 'Здравствуйте. У пользователя ' + name + ' новый пост - ' \
+           + '"' + title + '"' + '. С уважением, администрация проекта!'
     for i in subs:
         uuser = User.objects.get(id=i.user_id).email
         send_mail('Новое видео', mesg, EMAIL_HOST_USER, [uuser])
